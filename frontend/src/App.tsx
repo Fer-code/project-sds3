98b6b2fad3a1a0e4ed952f8b71f07048c70a6e00
Fer-code <fernandamirandacursos@gmail.com>
2021-09-09 10:41:23 -0300

    Update title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <>{/*Abrindo fragment*/}
     <NavBar/>
     <div className="container">
-      <h1 className="text-primary py-3">Sou a</h1>
+      <h1 className="text-primary py-3">Dashboard de vendas</h1>
 
       <div className="row px-3">
           <div className="col-sm-6">
