080f5e40614b03b6ac7e5d1d87bd6fa372a91245
Fer-code <fernandamirandacursos@gmail.com>
2021-09-08 22:27:52 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import DataTable from "componets/dataTable";
+import Footer from "componets/Footer";
+import NavBar from "componets/navBar/Index";
 
 
 function App() {
   return (
-    <div>
+    <>{/*Abrindo fragment*/}
+    <NavBar/>
+    <div className="container">
       <h1 className="text-primary">Sou a</h1>
+      <DataTable/>
     </div>
+
+    <Footer/>
+    </>//Fechando fragnent
+  
   );
 }
 
