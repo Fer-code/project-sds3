a45ec8c501f7712118c1c8f55704b89458dc9215
Fer-code <fernandamirandacursos@gmail.com>
2021-09-11 21:41:25 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,9 @@
-import BarChart from "componets/BarChart";
-import DataTable from "componets/dataTable";
-import DonutChart from "componets/DonutChart";
-import Footer from "componets/Footer";
-import NavBar from "componets/navBar/Index";
+import Routes from "Routes";
 
 
 function App() {
   return (
-    <>{/*Abrindo fragment*/}
-    <NavBar/>
-    <div className="container">
-      <h1 className="text-primary py-3">Dashboard de vendas</h1>
-
-      <div className="row px-3">
-          <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Taxa de sucesso</h5>
-              <BarChart/>
-
-          </div>
-          <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Todas as vendas</h5>
-              <DonutChart/>
-
-          </div>
-
-      </div>
-
-      <div className="py-3">
-        <h4 className="text-primary">Todas as vendas</h4>
-
-      </div>
-
-      <DataTable/>
-    </div>
-
-    <Footer/>
-    </>//Fechando fragnent
+          <Routes />
   
   );
 }
---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,41 @@
+import BarChart from "componets/BarChart";
+import DataTable from "componets/dataTable";
+import DonutChart from "componets/DonutChart";
+import Footer from "componets/Footer";
+import NavBar from "componets/navBar/Index";
+
+const Dashboard = () => {
+    return (
+        <>{/*Abrindo fragment*/}
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de vendas</h1>
+
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+                        <BarChart />
+
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Todas as vendas</h5>
+                        <DonutChart />
+
+                    </div>
+
+                </div>
+                
+                <div className="py-3">
+                    <h4 className="text-primary">Todas as vendas</h4>
+
+                </div>
+
+                <DataTable />
+            </div>
+
+            <Footer />
+        </>//Fechando fragnent
+    );
+}
+
+export default Dashboard;
