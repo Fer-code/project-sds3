c8908ef67a9ce7a8e3c665239456ce9c28caa769
Fer-code <fernandamirandacursos@gmail.com>
2021-09-12 18:34:01 -0300

    BarChart integration

--- frontend/src/componets/BarChart/index.tsx
+++ frontend/src/componets/BarChart/index.tsx
@@ -1,6 +1,56 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess, SaleSum } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/request';
+
+//Type auxiliar para "series"
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []                   
+            }
+        ]
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSuccess[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
+
+            setChartData({ 
+                labels: {
+                    categories: myLabels
+                },
+                series: [
+                {
+                    name: "% Success",
+                    data: mySeries                   
+                }
+            ] });
+
+        });
+    } , []);
 
     const options = {
         plotOptions: {
@@ -10,7 +60,7 @@ const BarChart = () => {
         },
     };
     
-    const mockData = {
+    /*const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
         },
@@ -20,12 +70,12 @@ const BarChart = () => {
                 data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
             }
         ]
-    };
+    };*/
 
     return (
         <Chart
-            options={{ ...options, xaxis: mockData.labels}} //tres pontinhos pode adicionar mais valores, xaxis é o eixo x, mockData é um nome bem usado para dados de mentira
-            series={mockData.series} //Pegando os valores
+            options={{ ...options, xaxis: chartData.labels}} //tres pontinhos pode adicionar mais valores, xaxis é o eixo x, mockData é um nome bem usado para dados de mentira
+            series={chartData.series} //Pegando os valores
             type="bar" //Informa que é um gráfico de barras
             height="240"
         />
